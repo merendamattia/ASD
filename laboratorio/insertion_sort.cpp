b83957d3121d5ce371ed69f2f1706db226e952db
0xMere <merenda.s.mattia@gmail.com>
2023-02-20 15:37:51 +0100

    test commit da CLI

---
+++ laboratorio/insertion_sort.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <string.h>
+using namespace std;
+
+void insertion_sort(int *A, int n){
+	for(int i = 0; i < n; i++) {
+		for(int j = i - 1; j >= 0; j--) {
+			if(A[j] > A[j + 1])
+				swap(A[j], A[j + 1]);
+			else
+				break;
+		}
+	}
+}
+
+void swap(int &a, int &b){
+	int temp = a;
+	a = b;
+	b = temp;
+}
+
+void print_array(int *A, int n){
+	cout << endl;
+	for(int i = 0; i < n; i++)
+		cout << A[i] << " ";
+}
+
+int main(){
+	int n = 100;
+	int *A = new int[n];
+
+	for(int i = 0; i < n; i++)
+		A[i] = n - i;
+
+	print_array(A, n);
+
+	insertion_sort(A, n);
+
+	print_array(A, n);
+
+
+	return 0;
+}
