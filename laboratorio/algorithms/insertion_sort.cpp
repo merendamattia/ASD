4013e08a0d2e2a809b8a44cccb7560afbea46edc
0xMere <merenda.s.mattia@gmail.com>
2023-02-27 10:02:01 +0100

    Prima parte appunti lezione 27/02/2023 (@merendamattia)

--- laboratorio/algorithms/insertion_sort.cpp
+++
@@ -1,8 +0,0 @@
-void insertion_sort(int *A, int dim){
-	for(int i = 0; i < dim; i++) {
-		for(int j = i - 1; j >= 0; j--) {
-			if(A[j] > A[j + 1])
-				swap(A[j], A[j + 1]);
-		}
-	}
-}
--- laboratorio/main.cpp
+++ laboratorio/main.cpp
@@ -1,24 +1,30 @@
 #include <iostream>
+#include <time.h>
+
 #include "./utilities/utility.h"
-#include "./algorithms/insertion_sort.cpp"
+#include "./algorithms/insertion.cpp"
 
 using namespace std;
 
 /*
  * Compilazione: g++ main.cpp -o main
- * Esecuzione: ./main 100 (100 è la dimensione dell'array)
- * Per visualizzare il tempo di esecuzione: time ./main 100
+ * Esecuzione: ./main 100 1 (100 è la dimensione dell'array, 1 è il valore della variabile "details")
+ * Per visualizzare il tempo di esecuzione: time ./main 100 1
 */
 
 int main(int argc, char **argv){
+
+	srand(time(NULL)); // Inizializzo la generazione di numeri casuali
 	
 	// controllo argomenti
-	if (argc != 2) {
-		printf("Inserire un solo elemento dopo il file eseguibile %s\n",argv[0]);
+	if (argc != 3) {
+		printf("ERRORE %s\n",argv[0]);
 		return -1;
 	}
 
 	int dim = atoi(argv[1]); // parsing argomento
+
+	bool details = atoi(argv[2]);
 
 	int *A = new int[dim];
 
@@ -26,9 +32,10 @@ int main(int argc, char **argv){
 
 	//print_array(A, dim);
 
-	insertion_sort(A, dim);
+	insertion_sort(A, dim, details);
 
-	print_array(A, dim);
+	//print_array(A, dim);
 
+	delete[] A;
 	return 0;
 }
