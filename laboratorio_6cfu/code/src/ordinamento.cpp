5d52f51d8f4176882d8101e145cc9b6caf51e3e2
manueldiagostino <diagostinomanuel@gmail.com>
2023-03-13 15:30:47 +0100

    implementazione long-options @manueldiagostino

--- laboratorio_6cfu/code/src/ordinamento.cpp
+++ laboratorio_6cfu/code/src/ordinamento.cpp
@@ -10,10 +10,10 @@ void print_usage(char **argv) {
         cerr << "            n = k * max-dim / d, k=1 .. d\n";
         cerr << "  -t=<int>: Specifica quanti volte viene lanciato l'algoritmo per una specifica dimensione n [default: 1]\n";
         cerr << "            Utile nel caso in cui l'input viene inizializzato in modo random\n";
-        cerr << "  -v [verbose]: Abilita stampe durante l'esecuzione dell'algoritmo\n";
-        cerr << "  -g [graph]: creazione file di dot con il grafo dell'esecuzione (forza d=1 t=1)\n";
-        cerr << "  -c [comparison]: (quicksort) compara la partition a tre vie classica con un'altra\n";
-        cerr << "  -o=[outputh path]: specifica un path per il file di dot\n";
+        cerr << "  -v, --verbose: Abilita stampe durante l'esecuzione dell'algoritmo\n";
+        cerr << "  -g, --graph: creazione file di dot con il grafo dell'esecuzione (forza d=1 t=1)\n";
+        cerr << "  -c, --comparison: (quicksort) compara la partition a tre vie classica con un'altra\n";
+        cerr << "  -o, --output-file: specifica un path per il file di dot\n";
 }
 
 
@@ -23,7 +23,51 @@ int parse_cmd(int argc, char **argv, Stat& s) {
         return 1;
     }
 
+    int option_index = 0;
+    static struct option long_options[] = {
+        {"graph"        , no_argument       , 0,    'g'},
+        {"verbose"      , no_argument       , 0,    'v'},
+        {"comparison"   , no_argument       , 0,    'c'},
+        {"output-file"  , required_argument , 0,    'o'}
+    };
+
     int c;
+    while ((c = getopt_long(argc, argv, ":d:t:o:vgc", long_options, &option_index)) != -1) {
+        switch (c) {
+        case 'v':
+            s.details = true;
+            break;
+        case 'c':
+            s.comparison = true;
+            break;
+        case 'g':
+            s.graph = 1;
+            s.ndiv = 1;
+            s.ntests = 1;
+            break;
+        case 'd':
+            s.ndiv = atoi(optarg);
+            break;
+        case 't':
+            s.ntests = atoi(optarg);
+            break;
+        case 'o':
+            s.output_path = std::string(optarg);
+            break;
+        case ':':
+            std::cerr << "Option -" << (char)optopt << " needs an argument." << std::endl;
+            print_usage(argv);
+            return 1;
+        case '?':
+            std::cerr << "Unknown option -" << (char)optopt << std::endl;
+            print_usage(argv);
+            return 1;
+        default:
+            break;
+        }
+    }
+
+    /* 
     while ((c = getopt(argc, argv, ":d:t:o:vgc")) != -1) {
         switch (c) {
         case 'v':
@@ -59,7 +103,12 @@ int parse_cmd(int argc, char **argv, Stat& s) {
         }
     }
 
-	s.max_dim = atoi(argv[optind]);
+	s.max_dim = atoi(argv[optind]); 
+    */
+    if (optind == argc-1) {
+	    s.max_dim = atoi(argv[optind++]); 
+    } else
+        print_usage(argv);
 
     if (s.ndiv > s.max_dim) {
         std::cerr << "-d argument must be less or equal to max-dim" << std::endl;
